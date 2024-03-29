23c2bc016b2178ce3757ee2b9cc025436c8ebf31
Angl99 <anggelplasencia@pursuit.org>
2024-03-29 16:31:45 -0400

    working on refactoring map/ implementation

--- frontend/src/Components/Map/Maps.jsx
+++ frontend/src/Components/Map/Maps.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Maps() {
-    useEffect(() => {
-        const mapBoxApiKey= import.meta.env.REACT_APP_MAPBOX_API_KEY;
-        console.log(process.env.REACT_APP_MAPBOX_API_KEY);
+    const mapBoxApiKey = import.meta.env.REACT_APP_MAPBOX_API_KEY;
+    const [map, setMap] = useState(null);
 
+    useEffect(() => {
         mapboxgl.accessToken = mapBoxApiKey;
-        const map = new mapboxgl.Map({
+
+        const newMap = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
-            // center: [-74.5, 40], // Example center coordinates
-            // zoom: 9 // Example zoom level
         });
 
-    }, []); 
+        setMap(newMap);
+
+        // Clean up the map instance on component unmount
+        return () => newMap.remove();
+    }, [mapBoxApiKey]);
 
     return (
-        <div id='map' style={{ width: '400px', height: '300px' }}></div>
+        <div>
+            <div id="map" style={{ width: '400px', height: '300px' }} />
+        </div>
     );
 }
 
